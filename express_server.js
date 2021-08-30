04cda3188ad2d32a1a346ea88a6c373a4181a055
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-30 16:44:21 -0700

    Check for absence of http or any other protocal suffix in longURL and auto appension of http added in express_server

--- express_server.js
+++ express_server.js
@@ -13,6 +13,8 @@ const generateRandomString = function() {
   return randStr;
 }
 
+const schemeNegCheck = /^([A-Za-z]+.)+[A-Z-a-z]+(\/?$|\/.+$)/; // Checks if the URL has a scheme/protocol specified
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
@@ -22,6 +24,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// GET requests
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -39,27 +42,47 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  if (req.body.longURL) {
-    const shortURL = generateRandomString();
-    urlDatabase[shortURL] = req.body.longURL;
-    // const templateVars = { shortURL: randURL, longURL: req.body.longURL };
-    // res.render('urls_show', templateVars);
-    res.redirect(`/urls/${shortURL}`);
-  } else {
-    res.send('Okay!');
-  }
-});
-
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req.params.shortURL);
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+// POST requests
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  if (req.body.longURL) {
+    let longURL;
+    if (schemeNegCheck.test(req.body.longURL)) {
+      longURL = 'http://' + req.body.longURL;
+    } else {
+      longURL = req.body.longURL;
+    }
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = longURL;
+    res.redirect(`/urls/${shortURL}`);
+  } else {
+    res.send('Okay!');
+  }
+});
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
