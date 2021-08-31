f3f92c9820b25ff182b4ce9c216bd797f97b7259
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-30 23:43:46 -0700

    username cookie clearing implemented upon user logout action in express_server

--- express_server.js
+++ express_server.js
@@ -105,6 +105,13 @@ app.post("/login", (req, res) => {
   res.redirect('/urls');
 });
 
+// Clearing user cookie per user log out and redirecting to /urls 
+app.post("/logout", (req, res) => {
+  console.log(req.body);
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
